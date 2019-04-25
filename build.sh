4918ed124306479fca4b9f875df8bf3e0f0bfaa8
Ante0 <ante00@gmail.com>
2019-04-25 21:04:50 +0200

    update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@
 #export CROSS_COMPILE=aarch64-linux-android-
 #export PATH=$PATH:$AARCH64_COMPILR
 make ARCH=arm64 O=out/ carbon_oneplus6_defconfig
-make ARCH=arm64 O=out/ -j8
+make ARCH=arm64 O=out/ -j`nproc`
